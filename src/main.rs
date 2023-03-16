bdcf56f58195611c45bae0a25cc86de15a7317ad
Kenny Zhuo Ming Lu <luzhuomi@gmail.com>
2023-03-16 22:27:49 +0800

    -updates

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,8 @@ fn main() {
 
 use std::io;
 
-fn main() {
-    println!("Guess the number!");
+/* fn main() {
+x    println!("Guess the number!");
 
     println!("Please input your guess.");
 
@@ -19,5 +19,46 @@ fn main() {
 
     println!("You guessed: {guess}");
 
+    foo();
+
+    
+
 }
 
+*/
+
+fn foo() {
+    let mut x = String::from("hello");
+
+    let r1 = bar(&x);
+    let r2 = &x;
+    println!("{r1} {r2}");
+}
+
+fn bar(s:&String) -> String {
+    let t = s.to_string() + "bye";
+    t // &t wont work as it will leak t out of scope
+}
+
+fn multref() {
+    let mut x = String::from("hello");
+    let r1 = &mut x;
+    let r2 = &x; 
+    // print!("{r1} {r2}"); // error, extending r1's life time here., x2 cant borrow
+}
+
+/*
+fn main() {
+    foo();
+}
+
+*/
+struct AlwaysEqual;
+
+struct Another;
+
+
+fn main() {
+    let x = AlwaysEqual;
+    let y = AlwaysEqual;
+}
