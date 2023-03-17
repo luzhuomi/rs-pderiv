2f9096f64e8aae7979e312cd00df3b2e94d239c9
Kenny Zhuo Ming Lu <luzhuomi@gmail.com>
2023-03-17 22:25:58 +0800

    updated

--- src/regex/re.rs
+++ src/regex/re.rs
@@ -7,27 +7,69 @@ pub enum RE {
     Star(Box<RE>),
 }
 
-pub fn pderiv(r:&RE, l:&char) -> Vec<RE> {
+impl Clone for RE {
+    
+    fn clone(&self) -> Self {
+        match self {
+            RE::Eps => RE::Eps,
+            RE::Lit(l) => RE::Lit(l.clone()),
+            RE::Seq(r1, r2) => RE::Seq(r1.clone(), r2.clone()),
+            RE::Choice(r1, r2) => RE::Choice(r1.clone(), r2.clone()),
+            RE::Star(r) => RE::Star(r.clone())
+        }
+    }
+}
+
+
+pub fn pderiv(r:&RE, l:&char) -> Vec<Box<RE>> {
     match r {
-        RE::Eps => Vec::new(),
+        RE::Eps => vec![],
         RE::Lit(m) => { 
-            if l == m { Vec::from([RE::Eps])} else { Vec::new() }
+            if l == m { 
+                vec![Box::new(RE::Eps)]
+            } else { 
+                vec![]
+            }
         }
         RE::Seq(r1, r2) => { 
-            let ts = pderiv(r1,l);
-            let r = Vec::new();
-            for t in &ts {
-                r.push(RE::Seq(t,r2));
-            };
-            r
-            // Vec::new() // fixme
+            if nullable(r1) {
+                let ts =  pderiv(r1,l);
+                let vs =  pderiv(r2,l);
+                let mut res = vec![];
+                for t in ts {
+                    res.push(Box::new(RE::Seq(t, r2.clone())));                    
+                };
+                for v in vs {
+                    res.push(v);
+                }
+                res
+
+            } else {
+                let ts = pderiv(r1,l);
+                let mut res = Vec::new();
+                for t in ts {
+                    res.push(Box::new(RE::Seq(t, r2.clone())));
+                };
+                res    
+            }
         }
         RE::Choice(r1,r2) => {
-            Vec::new() // fixme
+            let ts = pderiv(r1,l);
+            let vs = pderiv(r2,l);
+            let mut res = vec![];
+            for t in ts { res.push(t) };
+            for v in vs { res.push(v) };
+            res
         },
-        RE::Star(r) => {
-            Vec::new() // fixme
+        RE::Star(r1) => {
+            let ts = pderiv(r1, l);
+            let mut res = vec![];
+            for t in ts {
+                res.push(Box::new(RE::Seq(t, Box::new(r.clone()))));    
+            }
+            res
         }
+        
     }
 }
 
