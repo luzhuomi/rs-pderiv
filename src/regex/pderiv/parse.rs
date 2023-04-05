defc2cda1b2e42bd00983064ebedcf65c79fee9c
Kenny Zhuo Ming Lu <luzhuomi@gmail.com>
2023-04-05 14:01:10 +0800

    replacing recursive call in build_fix() by while loop, tail recursion is not guaranteed in rust

--- src/regex/pderiv/parse.rs
+++ src/regex/pderiv/parse.rs
@@ -35,40 +35,11 @@ pub fn cnt(regex:&Regex)-> usize {
             2  dom(dom(currTrans)) \in allStatesFoFar
             3  all_states_sofar_im is the  intmap representation of all_states_sofar for quicker lookup
  */
-fn build_fix(mut cache: PDCached, mut all_states_sofar: Vec<RE>, mut all_states_sofar_im: IntMap<()>, new_states: Vec<RE>, curr_trans:Trans, sig:HashSet<char>) -> (Vec<RE>, Trans) {
-    if new_states.len() == 0 {
-        (all_states_sofar, curr_trans)
-    } else {
-        /* 
-        let new_states_clone = new_states.clone(); // todo, this clone is expensive when states are huges, e.g. 10k
-        let new_delta:Vec<_> = new_states_clone.iter().flat_map(
-            |r| {
-                let e = sig.iter().filter(|l|{
-                    let hash_r = calculate_hash(r);
-                    let hash_l = calculate_hash(l);
-                    let key = hash2(&hash_r, &hash_l);
-                    !(curr_trans.contains_key(key))
-                }).flat_map(move |l| {
-                    let tfs = pderiv_bc(r,l);
-                    if tfs.len() == 0 {
-                        None
-                    } else {
-                        let tfdelta = tfs;
-                        Some((r,l,tfdelta))    
-                    }
-                });
-                e
-            }).collect();
-        */
+fn build_fix(mut cache: PDCached, mut all_states_sofar: Vec<RE>, mut all_states_sofar_im: IntMap<()>, mut new_states: Vec<RE>, mut curr_trans:Trans, sig:HashSet<char>) -> (Vec<RE>, Trans) {
+    while new_states.len() > 0 {
         let (new_states_clone, new_data_nested):(Vec<_>, Vec<_>) = new_states.into_iter().map(
             |r| {
                 let e:Vec<_> = sig.iter()
-                /* .filter(|l|{
-                    let hash_r = calculate_hash(&r);
-                    let hash_l = calculate_hash(l);
-                    let key = hash2(&hash_r, &hash_l);
-                    !(curr_trans.contains_key(key))
-                })*/
                 .flat_map(|l| {
                     let l_c = l.clone();
                     let tfs = cache.pderiv_bc(&r,&l_c);
@@ -112,9 +83,13 @@ fn build_fix(mut cache: PDCached, mut all_states_sofar: Vec<RE>, mut all_states_
         });
         // dbg!(&new_states_next.len());
         all_states_sofar.extend(new_states_clone);
-        let all_states_next:Vec<RE> = all_states_sofar;
-        build_fix(cache, all_states_next, all_states_next_im, new_states_next, next_trans, sig)
+        // let all_states_next:Vec<RE> = all_states_sofar;
+        all_states_sofar_im = all_states_next_im;
+        new_states = new_states_next;
+        curr_trans = next_trans;
+        // build_fix(cache, all_states_next, all_states_next_im, new_states_next, next_trans, sig)
     }
+    (all_states_sofar, curr_trans)
 }
 
 
@@ -135,126 +110,6 @@ pub fn build_regex(r:&RE) -> Regex { // todo: GET RID OF ALL_STATES, WHICH IS ON
     Regex{trans : trans, init : r, finals:fins}
 }
 
-// this could be inefficient.
-/* 
-fn build_fix(all_states_sofar: HashSet<RE>,  curr_trans:Trans, sig:HashSet<char>) -> (HashSet<RE>, Trans) {
-    let mut new_delta = all_states_sofar.iter().flat_map(
-        |r| {
-            let e = sig.iter().filter(|l| {
-                let hash_r = calculate_hash(r);
-                let hash_l = calculate_hash(l);
-                let key = hash2(&hash_r, &hash_l);
-                !(curr_trans.contains_key(key))
-            }).flat_map(move |l| {
-                let tfs = pderiv_bc(r, l);
-                if tfs.len() == 0 {
-                    None
-                } else {
-                    let tfdelta = tfs.into_iter();
-                    Some((r,l,tfdelta))    
-                }
-            });
-            e
-        }).peekable();
-    if new_delta.peek().is_none() { 
-        (all_states_sofar, curr_trans)
-    } else {
-        let new_trans :Trans = curr_trans.clone();
-        let (mut next_states, next_trans) = new_delta
-            .fold((HashSet::new(), new_trans), |acc, t| {
-                let (states_sofar, mut trans) = acc;
-                let (src, c, dstbvs) = t;
-                let hash_src = calculate_hash(src);
-                let hash_c  = calculate_hash(c);
-                let key = hash2(&hash_src, &hash_c);
-                let states_out = dstbvs.clone().fold(states_sofar, |mut states, s| {
-                    let (dst, _bv) = s;
-                    let _inserted = states.insert(dst);
-                    states
-                });
-                trans.insert(key, dstbvs.into_iter().map(|(dst, bv)| (calculate_hash(&dst), bv)).collect());
-                (states_out, trans)
-        });
-        next_states.extend(all_states_sofar); // todo check why all_states_sofar can't be use as the init of next_states fold.
-        build_fix(next_states,next_trans, sig)
-    }
-}
-
-pub fn build_regex(r:&RE) -> Regex {
-    let sig = r.sigma();
-    let init_states = vec![r.clone()].into_iter().collect();
-    let (all_states, trans) = build_fix(init_states, IntMap::new(), sig);
-    let emp_im :IntMap<BitVec> = IntMap::new();
-    let fins = all_states.iter().filter(|r|{r.nullable()}).fold(emp_im, |mut im,r|{
-        let hash_r = calculate_hash(r);
-        im.insert(hash_r, emp_code(r));
-        im
-    });
-    Regex{trans : trans, init : r, finals:fins}
-}
-*/
-
-
-/*
-impl <'a> Regex<'a> {
-    pub fn parse_regex(&self, s:&'a String) -> Option<BitVec> {
-        fn go<'a>(rbc:Vec<(u64,BitVec)>, trans:&Trans, finals:&Finals, s:&str) -> Vec<BitVec> {
-            if s.len() == 0 {
-                let mut res:Vec<BitVec> = vec![];
-                rbc.into_iter().for_each(|(r, bc)| { // this is inefficient, we are building all possible final states, but we only need one.
-                    match finals.get(r) {
-                        None => {}
-                        Some(bc1) => {
-                            let mut bc2 = bc.clone();
-                            bc2.extend(bc1);
-                            res.push(bc2);
-                        }
-                    }
-                });
-                res
-            } else {
-                let ox = &s[0..1].chars().nth(0);
-                let (x,xs) = match ox {
-                    None => panic!("parse_regex failed, empty string slice with len > 0"),
-                    Some(c) => (c,&s[1..])
-                };
-                let mut tbc:Vec<(u64, BitVec)> = vec![];
-                rbc.into_iter().for_each(|(r,bc)| {
-                    let hash_r = r;
-                    let hash_x = calculate_hash(x);
-                    let key = hash2(&hash_r, &hash_x);
-                    match trans.get(key) {
-                        None => {
-                        }
-                        Some(tfs) => tfs.into_iter().for_each(|tb|{
-                            let (t, bc1) = tb;
-                            let mut bc2 = bc.clone(); // this is inefficient, we need to clone the current accumulated bc for the next N possible states, but in the end we only need one1
-                            bc2.extend(bc1);
-                            tbc.push((*t,bc2));
-                        })
-                    };
-                });
-                tbc = nub_vec_fst_u64(tbc);
-                go(tbc, trans, finals, xs)
-            }
-        }
-    
-        match self {
-            Regex { trans, init, finals } => {
-                let hash_init = calculate_hash(init);
-                let result =  go(vec![(hash_init,BitVec::new())], trans, finals, &s);
-                if result.len() == 0 {
-                    None
-                } else {
-                    let bv: BitVec = (result.clone()[0]).clone();
-                    Some(bv)
-                }
-            }
-        }
-    
-    }
-}
- */
 
 
 // One way to speed up is to keep track of the select sequence bitvec in reversed order.
